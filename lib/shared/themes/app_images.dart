d66a86e36b33f0d9869049940603098deccd4d26
JohnnyFreire97 <johnnyfreirejh@hotmail.com>
2021-08-02 22:12:46 -0300

    feature:  create app theme

---
+++ lib/shared/themes/app_images.dart
@@ -0,0 +1,10 @@
+class AppImages {
+  static const _assetPng = 'assets/images';
+
+  //ASSETS
+  static const logoFull = '$_assetPng/logofull.png';
+  static const logoMini = '$_assetPng/logomini.png';
+  static const union = '$_assetPng/union.png';
+  static const person = '$_assetPng/person.png';
+  static const google = '$_assetPng/google.png';
+}
